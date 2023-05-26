9c00e7f44744de181dacc99ea4e97f26e9107cf9
faizan-99 <mfaizy605@gmail.com>
2023-05-26 15:18:46 +0500

    updated

--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -3,40 +3,31 @@ import "../Contact/ContactForm.css";
 
 const ContactForm = () => {
   return (
-    <section class="Material-contact-section section-padding section-dark">
+    <section class="section-padding section-dark">
       <div class="container">
         <div class="row">
-          <div class="col-md-12">
-            <h5 class="section-title">Contact Us</h5>
-            <h3>REACH US OUT</h3>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6 mt-3 contact-widget-section2">
-            <p>
+          <div class="col-md-6 mt-3">
+            <h4 class="section-title">CONTACT US</h4>
+            <h2 class="section-subtitle">REACH US OUT</h2>
+            <p class="section-description">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
             </p>
 
-            <div class="find-widget">
-              Dubai: <a>United Arab Emirates</a>
-            </div>
-            <div class="find-widget">
-              Sharjah: <a>United Arab Emirates</a>
-            </div>
+            <div class="find-widget">Dubai, United Arab Emirates</div>
+            <div class="find-widget">Sharjah, United Arab Emirates</div>
           </div>
           <div class="col-md-6">
             <form
               class="shake"
               role="form"
-              method="post"
+              // method="post"
               id="contactForm"
               name="contact-form"
-              data-toggle="validator"
             >
-              <div class="form-group label-floating">
+              <div class="form-group">
                 <label class="control-label" for="email">
                   Your Email Goes Here
                 </label>
@@ -48,7 +39,7 @@ const ContactForm = () => {
                 />
               </div>
 
-              <div class="form-group label-floating">
+              <div class="form-group">
                 <label for="message" class="control-label">
                   Type Your Message Here
                 </label>
@@ -60,8 +51,8 @@ const ContactForm = () => {
                 ></textarea>
               </div>
 
-              <div class="form-submit mt-5">
-                <button class="btn btn-common">Send</button>
+              <div class="form-submit mt-5 text-end">
+                <button class="btn-send">SEND</button>
               </div>
             </form>
           </div>
