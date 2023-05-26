6da5f7886e8d1f76ad1794ec7f28450b367c9a98
faizan-99 <mfaizy605@gmail.com>
2023-05-26 18:25:21 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import ContactForm from "./Components/Contact/ContactForm";
+
 import EthicalEssence from "./Components/EthicalEssence/EthicalEssence";
 import NavigationBar from "./Components/Miscellaneous/NavigationBar";
+import Portfolio from "./Components/Portfolio/Portfolio";
 import ServiceSlider from "./Components/Service/ServiceSlider";
 import WorkFlow from "./Components/WorkFlow/WorkFlow";
 
@@ -12,7 +14,9 @@ function App() {
       {/* <WorkFlow /> */}
       {/* <EthicalEssence /> */}
       {/* <ServiceSlider /> */}
-      <ContactForm />
+      {/* <ContactForm /> */}
+
+      <Portfolio />
     </div>
   );
 }
---
+++ src/Components/Portfolio/Portfolio.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import imgd1 from "../../assets/PortfolioImages/400-x-400.jpg";
+import imgd2 from "../../assets/PortfolioImages/400-x-550.jpg";
+
+const Portfolio = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    cssEase: "linear",
+    centerMode: true,
+    centerPadding: "50px",
+  };
+  return (
+    <Slider {...settings} className="py-5">
+      <img src={imgd1} className="my-5 px-4" />
+      <img src={imgd2} className="px-4" />
+      <img src={imgd1} className="my-5 px-4" />
+      <img src={imgd2} className="px-4" />
+      <img src={imgd1} className="my-5 px-4" />
+      <img src={imgd2} className="px-4" />
+    </Slider>
+  );
+};
+
+export default Portfolio;
